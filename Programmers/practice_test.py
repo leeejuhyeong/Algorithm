ceffb986c20ce8bd99ff8d30f9da8add3ced97c3
leeejuhyeong <wnguddl827@gmail.com>
2021-05-14 14:46:36 +0900

    add programmers 모의고사's solution

--- Programmers/practice_test.py
+++ Programmers/practice_test.py
@@ -1,56 +1,22 @@
 def solution(answers):
     answer = []
-    lenth = len(answers)
+    a = [1, 2, 3, 4, 5]
+    b = [2, 1, 2, 3, 2, 4, 2, 5]
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
 
-    first, second, third = 0, 0, 0
-    i = 1
-    for k in answers:
-        if i == 6:
-            i = 1
-        if i == k:
-            first += 1
-        i += 1
 
-    second_flag = 0
-    second_list = [1, 3, 4, 5]
-    for i, k in enumerate(answers):
-        if i % 2 == 0:
-            if k == 2:
-                second += 1
-        else:
-            if second_flag == 4:
-                second_flag = 0
-            if second_list[second_flag] == k:
-                second += 1
-            second_flag += 1
+    for idx, i in enumerate(answers):
+        if i == a[idx % 5]:
+            score[0] += 1
+        if i == b[idx % 8]:
+            score[1] += 1
+        if i == c[idx % 10]:
+            score[2] += 1
 
-    third_flag = 0
-    third_return = 0
-    third_list = [3, 1, 2, 4, 5]
-
-    for k in answers:
-        if third_flag == 2:
-            third_return += 1
-            third_flag = 0
-        if third_return == 5:
-            third_return = 0
-        if third_list[third_return] == k:
-            third += 1
-        third_flag += 1
-
-    Q = [[first, 1], [second, 2], [third, 3]]
-    Q.sort(key=lambda x:x[0], reverse=True)
-
-    if Q[0][0] == Q[1][0]:
-        if Q[0][0] == Q[2][0]:
-            answer.append(Q[0][1])
-            answer.append(Q[1][1])
-            answer.append(Q[2][1])
-        else:
-            answer.append(Q[0][1])
-            answer.append(Q[1][1])
-    else:
-        answer.append(Q[0][1])
+    for idx, i in enumerate(score):
+        if i == max(score):
+           answer.append(idx+1)
 
     return answer
 
