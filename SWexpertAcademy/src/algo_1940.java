05afaebe5ff1705a86b00160d7f9f8164b47483c
leeejuhyeong <wnguddl827@gmail.com>
2021-08-10 22:05:04 +0900

    add SWexpertAcademy_1940 가랏! RC카!'s solution

--- SWexpertAcademy/src/algo_1940.java
+++ SWexpertAcademy/src/algo_1940.java
@@ -2,26 +2,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class algo_1940 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= t; test_case++){
+        for (int test_case = 1; test_case <= t; test_case++) {
             int n = Integer.parseInt(br.readLine());
             int result = 0;
             int now_accel = 0;
 
-            for(int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 String[] input = br.readLine().split(" ");
                 int gear = Integer.parseInt(input[0]);      // 1 : 가속, 2 : 감속
                 int accel = 0;                              // 가속도
-                if(gear != 0) accel = Integer.parseInt(input[1]);  // 0일때 현재속도 유지
+                if (gear != 0) accel = Integer.parseInt(input[1]);  // 0일때 현재속도 유지
 
-                if(gear == 1){
-                    now_accel += accel;
-                } else if(gear == 2){
-                    now_accel -= accel;
-                    if(now_accel < 0) now_accel = 0;
+                switch (gear) {
+                    case 1:
+                        now_accel += accel;
+                        break;
+                    case 2:
+                        now_accel -= accel;
+                        if (now_accel < 0) now_accel = 0;
+                        break;
                 }
                 result += now_accel;
             }
