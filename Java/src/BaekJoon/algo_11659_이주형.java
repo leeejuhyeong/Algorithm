2a4a5db31b8b158c8fe7fe669ada5e784b4ed995
leeejuhyeong <wnguddl827@gmail.com>
2022-01-23 19:08:01 +0900

    add BaekJoon 구간 합 구하기 4's solution

---
+++ Java/src/BaekJoon/algo_11659_이주형.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class algo_11659_이주형 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());     // 1번부터 n번 수까지 더하면서 저장
+        }
+
+        int start, end;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
+
+            bw.write(arr[end] - arr[start - 1] + "\n");     // start번째 배열에 저장된 값까지 더하는 것이므로 start-1을 빼줌
+        }
+        bw.flush();
+        bw.close();
+    }
+}
